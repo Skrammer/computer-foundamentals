c396d035ce3d111a062c36f11f9ba285eb93a051
Zhalgas Zhumabayev <skrammer777@gmail.com>
2016-11-28 20:35:25 +0600

    Add files via upload

---
+++ bash-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir audio
+mkdir images
+mkdir video
+mkdir text
+
+cd text
+mkdir phone
+mkdir comp
+cd ../
+
+cd audio
+mkdir phone
+mkdir comp
+cd ../
+
+cd video
+mkdir phone
+mkdir comp
+cd ../
+
+cd images
+mkdir phone
+mkdir comp
+cd ../
